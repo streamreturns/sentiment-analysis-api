effb68d130da94b19859d1ea936b262bc78d5ed4
Byeongho Kang <byeongho.kang@yahoo.com>
2020-10-23 08:32:31 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,84 @@
-# sentiment-analysis-api
+# Sentiment Analysis API
+Deep Learning based Sentiment Analysis from Surprise to Anger (S2A)
+
+## Usage (Python 3)
+### Import Library
+```
+import requests
+```
+
+### Set API Host
+```
+api_host = 'fastapi.kr'
+api_port = '1128'
+```
+
+### [Example] Positive Sentiment
+- **Request**
+```
+text = '봄날에 입고가기 좋아요'
+r = requests.post('http://{api_host}:{api_port}/sentiment_analysis'.format(api_host=api_host, s2a_port=int(api_port)), data={'text': text})
+```
+
+- **Response**
+```
+{'raw': [-3.826526641845703, 3.8188369274139404],
+ 'sentiment': 'positive',
+ 'softmax': [0.00047802767949178815, 0.9995219707489014]}
+```
+
+### [Example] Negative Sentiment
+- **Request**
+```
+text = '재질이 거칠어요'
+r = requests.post('http://{api_ip}:{s2a_port}/sentiment_analysis'.format(api_ip=api_ip, s2a_port=int(s2a_port)), data={'text': text})
+```
+
+- **Response**
+```
+{'raw': [2.857180118560791, -2.8349578380584717],
+ 'sentiment': 'negative',
+ 'softmax': [0.9966388940811157, 0.0033610411919653416]}
+```
+
+### [Example] Bulk Inference
+This example analyzes `text` column in the `target_df`.
+
+- **Define Bulk Inference Method `analyze_sentiment`**
+```
+def analyze_sentiment(row):
+    text = prettify_string(row['text'])
+    
+    r = requests.post('http://{api_ip}:{s2a_port}/sentiment_analysis'.format(api_ip=api_ip, s2a_port=int(s2a_port)),
+                      data={
+                          'text': text
+                      })
+    
+    if r.ok:
+        resp = r.json()
+        row['sentiment'] = resp['sentiment']
+        row['raw'] = numpy.max(resp['raw'])
+        row['softmax'] = numpy.max(resp['softmax'])
+    else:
+        row['sentiment'] = 'API error'
+        row['raw'] = 'API error'
+        row['softmax'] = 'API error'    
+        
+    return row
+```
+
+- **Bulk Inference**
+```
+target_df = target_df.apply(lambda x: analyze_sentiment(x), axis=1)
+```
+
+### Supported Languages
+
+Korean (한국어)
+
+---
+
+- **Maintainer**
+Byeongho Kang (byeongho.kang@yahoo.com)
+- **GitHub**
+https://github.com/streamreturns
---
+++ sentiment_analysis_api_server.py
@@ -0,0 +1,96 @@
+import torch, os
+from flask import Flask, json, request, redirect
+from flask_cors import CORS
+from transformers import ElectraConfig, ElectraTokenizer, ElectraForSequenceClassification
+
+# settings
+model_path = 'koelectra-base-v3-sentiment-model'
+max_length = 256
+labels = ['0', '1']
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+device = 'cpu'
+print('use Torch Device `{}`'.format(device))
+
+config = ElectraConfig.from_pretrained(
+    model_path,
+    num_labels=len(labels),
+    id2label={str(i): label for i, label in enumerate(labels)},
+    label2id={label: i for i, label in enumerate(labels)},
+)
+
+# load tokenizer
+tokenizer = ElectraTokenizer.from_pretrained(
+    model_path,
+    do_lower_case=False
+)
+
+# load & deploy model
+model = ElectraForSequenceClassification.from_pretrained(model_path, config=config)
+model.to(device)
+
+print('Fine-tuned BertClassificationModel `{}` loaded.'.format(model_path))
+print('> Location: `{}`'.format(os.path.abspath(model_path)))
+
+sentiment_analysis_api = Flask(__name__)
+
+# set CORS origins
+sentiment_analysis_api.config['CORS_HEADERS'] = 'Content-Type'
+cors = CORS(sentiment_analysis_api, resources={r'/*': {'origins': '*'}})
+
+# set max content length
+sentiment_analysis_api.config['MAX_CONTENT_LENGTH'] = 2048 * 1024 * 1024
+
+
+def infer(text):
+    encoded = tokenizer.encode_plus(
+        text,
+        max_length=max_length,
+        padding='max_length',
+        add_special_tokens=True,
+        truncation=True
+    )
+
+    input_ids = torch.unsqueeze(torch.tensor(encoded['input_ids'], dtype=torch.long).to(device, dtype=torch.long), dim=0)
+    attention_mask = torch.unsqueeze(torch.tensor(encoded['attention_mask'], dtype=torch.long).to(device, dtype=torch.long), dim=0)
+    token_type_ids = torch.unsqueeze(torch.tensor(encoded['token_type_ids'], dtype=torch.long).to(device, dtype=torch.long), dim=0)
+    outputs = model(input_ids, attention_mask, token_type_ids)
+
+    sentiment = 'positive' if int(outputs[0].argmax().data) == 1 else 'negative'
+    raw = torch.squeeze(outputs[0]).tolist()
+    softmax = torch.squeeze(torch.softmax(outputs[0], 1)).tolist()
+
+    return {
+        'sentiment': sentiment,
+        'raw': raw,
+        'softmax': softmax
+    }
+
+
+@sentiment_analysis_api.route('/sentiment_analysis', methods=['POST'])
+def sentiment():
+    print('POST `sentiment()`')
+    if request.method == 'POST':
+        # print('[POST]', request.values)
+
+        if 'text' in request.values:
+            text = request.values['text']
+        else:
+            text = ''
+
+        inferred = infer(text)
+        print(inferred)
+
+        response = sentiment_analysis_api.response_class(
+            response=json.dumps(inferred),
+            status=200,
+            mimetype='application/json',
+        )
+
+        response.headers['Access-Control-Allow-Origin'] = '*'
+
+        return response
+
+
+if __name__ == '__main__':
+    sentiment_analysis_api.run(host='0.0.0.0', port=1129, debug=False)
